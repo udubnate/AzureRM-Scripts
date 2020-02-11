439fbefcdff4dee4ac039c45d2c9ee583c6e50ee
Nate Bruneau <nathbr@microsoft.com>
2020-02-11 11:34:07 -0800

    Check in release of Remove-AllResourceGroups

--- Remove-AllResourceGroups.ps1
+++ Remove-AllResourceGroups.ps1
@@ -1,2 +1,23 @@
 # Clean up script
 # Remove all Resource Groups from test collection
+
+# Install AzureRM cmdlets
+
+#Connect interactively (check if existing connection)
+
+$ARMContext =    Get-AzureRmContext -ErrorAction Continue
+if (-not $ARMContext){
+    Connect-AzureRmAccount
+} else {
+    Write-Output $ARMContext
+}
+
+
+#Get all resource groups
+$ResourceGroups = Get-AzureRmResourceGroup | Select-Object -ExpandProperty ResourceGroupName
+
+#Remove-RMResourceGroup 
+
+foreach ($rg in $ResourceGroups){
+    Remove-AzureRmResourceGroup -name $rg
+}
