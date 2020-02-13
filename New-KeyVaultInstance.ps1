1b6d261e0e8abd92f67f43d60fc31ad885c2de73
udubnate <natebruneau@gmail.com>
2020-02-13 08:49:26 -0800

    add features to add an secret deployment password

--- New-KeyVaultInstance.ps1
+++ New-KeyVaultInstance.ps1
@@ -1,3 +1,13 @@
 # Use ARM Template, but some specifics to KeyVault to capture - ie. Register KeyVault
 
 Register-AzResourceProvider -ProviderNamespace 'Microsoft.KeyVault'
+
+New-AzResourceGroup -Name ExampleGroup -Location centralus
+New-AzKeyVault `
+  -VaultName ExampleVault `
+  -resourceGroupName ExampleGroup `
+  -Location centralus `
+  -EnabledForTemplateDeployment
+  
+$secretvalue = ConvertTo-SecureString 'redacted' -AsPlainText -Force
+$secret = Set-AzKeyVaultSecret -VaultName ExampleVault -Name 'ExamplePassword' -SecretValue $secretvalue
